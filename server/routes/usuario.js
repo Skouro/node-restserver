bf2d62321078ea413ad60965263d4bef996031f6
skouro <restrepomesadiego@gmail.com>
2019-02-24 18:56:34 -0500

    Se agrego conexion a base de datos fisica

--- server/routes/usuario.js
+++ server/routes/usuario.js
@@ -40,38 +40,6 @@ app.get('/usuario', [verificaToken],(req, res) => {
 
         });
 });
-app.get('/usuarioo',(req, res) => {
-    // return res.json({
-    //     usuario: req.usuario,
-    //     nombre: req.usuario.nombre,
-    //     email: req.usuario.email,
-    // });
-    console.log(req.usuario);
-    let desde = req.query.desde || 0;
-    desde = Number(desde);
-    let limite = req.query.limite || 100;
-    limite = Number(limite);
-
-    Usuario.find({estado: true}, 'nombre email role estado google role img') // Permite excluir los valores que se quieren mostrar { campo con condicion}
-        .skip(desde)
-        .limit(limite)
-        .exec((err, usuarios) => {
-            if (err) {
-                return res.status(400).json({
-                    ok: false,
-                    err: err
-                });
-            }
-            Usuario.count({estado: true},(err, conteo) => {
-                res.json({
-                    ok: true,
-                    usuarios,
-                    conteo
-                });
-            });
-
-        });
-});
 
 app.post('/usuario',[verificaToken,verificaAdmin_Role], (req, res) => {
 
@@ -97,8 +65,6 @@ app.post('/usuario',[verificaToken,verificaAdmin_Role], (req, res) => {
         });
     });
 });
-
-
 
 app.put('/usuario/:id',[verificaToken,verificaAdmin_Role], (req, res) => {
     let id = req.params['id'];
@@ -139,31 +105,6 @@ app.delete('/usuario/:id',[verificaToken,verificaAdmin_Role], (req, res) => {
     });
 });
 
-//Insertar usuarios sin autenticacion
-app.post('/usuarioo', (req, res) => {
-
-    let body = req.body;
-
-    let usuario = new Usuario({
-        nombre: body.nombre,
-        email: body.email,
-        password: bcrypt.hashSync(body.password, 10),
-        role: body.role
-    });
-    usuario.save((err, usuarioDB) => {
-        if (err) {
-            return res.status(400).json({
-                ok: false,
-                err: err
-            });
-        }
-
-        res.json({
-            ok: true,
-            usuario: usuarioDB
-        });
-    });
-});
 module.exports = app;
 
 
