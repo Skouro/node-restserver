7a8c9de2af55188838a65fc60ad2146e8e279171
skouro <restrepomesadiego@gmail.com>
2019-02-24 19:07:17 -0500

    Se agrego conexion a base de datos fisica

--- server/config/config.js
+++ server/config/config.js
@@ -1,26 +1,38 @@
-//Puerto
+// ============================
+//  Puerto
+// ============================
 process.env.PORT = process.env.PORT || 3000;
-//Entorno
-process.env.Node_ENV  = process.env.Node_ENV  || 'dev';
-
-//Vencimiento del token
-//60 segundos
-//60 minutos
-//24 horas
-//30 dias
-process.env.CADUCIDA_TOKEN =  60*60*24*30;
-//Semilla del token
-// process.env.SEED = process.env.SEED  || 'chucho-el-cachucho-desarrollo';
-process.env.SEED = 'chucho-el-cachucho-desarrollo';
 
 
-//Base de datos
+// ============================
+//  Entorno
+// ============================
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+
+// ============================
+//  Vencimiento del Token
+// ============================
+// 60 segundos
+// 60 minutos
+// 24 horas
+// 30 días
+process.env.CADUCIDAD_TOKEN = 60 * 60 * 24 * 30;
+
+
+// ============================
+//  SEED de autenticación
+// ============================
+process.env.SEED = process.env.SEED || 'chucho-el-cachucho-desarrollo';
+
+// ============================
+//  Base de datos
+// ============================
 let urlDB;
 
-if (process.env.Node_ENV  === 'dev') {
-    urlDB = 'mongodb://localhost/cafe'; //Si estamos en un entorno local
-} else  {
-    // urlDB  =  process.env.MONGO_URL; //Si se encuentra en producción
-    urlDB  =  "mongodb+srv://admin:1193120855@cluster0-xjwrt.mongodb.net/cafe?retryWrites=true"; //Si se encuentra en producción
+if (process.env.NODE_ENV === 'dev') {
+    urlDB = 'mongodb://localhost:27017/cafe';
+} else {
+    urlDB = "mongodb+srv://admin:1193120855@cluster0-xjwrt.mongodb.net/cafe?retryWrites=true";
 }
 process.env.URLDB = urlDB;
